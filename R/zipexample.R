b99974e0dbb205de358a57e1397ae4f8ef732c9d
ebikei <ebikei@hotmail.com>
2016-01-15 10:43:10 -0800

    Create zipexample.R

---
+++ R/zipexample.R
@@ -0,0 +1,18 @@
+setwd("K:\\RSampleCode")
+x<-c("stringr","zipcode","dplyr")
+lapply(x, require, character.only=T)
+
+stateCntyCodes = read.table("georef02.txt", sep = ",", colClasses = c("character"),header = TRUE)
+zipCnty=read.table("zcta_county_rel_10.txt", sep = ",", colClasses = c("character"),header = TRUE)
+head(zipCnty)
+data(zipcode)
+head(zipcode)
+
+# merge zip-city with zip-county-state
+zipMap = merge(zipCnty[, c("ZCTA5", "STATE", "COUNTY")], zipcode[, c("zip","city")], by.x = "ZCTA5", by.y = "zip")
+# get names of county and state
+zipMap2 = merge(zipMap, stateCntyCodes, by.x = c("STATE", "COUNTY"), by.y = c("fipstate","fipscty"))
+zipMap2$stname = sapply(zipMap2$ctyname, function(x) str_split(x, ",")[[1]][2])
+head(zipMap2)
+save(zipMap2,file="zipCityCountyStateMap.Rda")
+
