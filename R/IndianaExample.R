cf0a1026a0995534f36b87877fdd783334ca9be9
ebikei <ebikei@hotmail.com>
2016-01-17 01:55:25 -0800

    Update IndianaExample.R

--- R/IndianaExample.R
+++ R/IndianaExample.R
@@ -1,4 +1,4 @@
-setwd("K:\\RSampleCode")
+setwd("F:\\Dropbox\\R Sample Code\\Data")
 x<-c("stringr","XML","plyr","RCurl","ggplot2","maptools","rgeos","ggmap","dplyr","RJSONIO")
 lapply(x, require, character.only=T)
 
@@ -9,8 +9,13 @@ cntyList=c("Marion County, IN","Hamilton County, IN","Boone County, IN","Hendric
 zipIndy=filter(zipMap2,ctyname %in% cntyList)
 zipIndy2=zipIndy[!duplicated(zipIndy$ZCTA5),]
 
-zipShp=readShapePoly("tl_2010_18_zcta500\\tl_2010_18_zcta500.shp")
-zipShp2=fortify(zipShp,region="ZCTA5CE00")
-
 zipShp=readShapePoly("tl_2010_18_zcta510/tl_2010_18_zcta510.shp")
 zipShp2=fortify(zipShp,region="ZCTA5CE10")
+
+# retain zips in Indy area
+zipShp3=zipShp2[zipShp2$id %in% zipIndy$ZCTA5,]
+
+x=get_googlemap(center="indianapolis",maptype=c("roadmap"))
+p=ggmap(x)
+p=p+geom_polygon(data=zipShp3,aes(x=long,y=lat,group=id),fill="blue",color="black",alpha=0.2)
+print(p)
